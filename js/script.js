1cc61274f0997fe31dbd6ffe22ba2ad847fb4212
mersad <mersad@gmail.com>
2024-04-23 11:56:04 +0330

    ascii code project created

---
+++ js/script.js
@@ -0,0 +1,27 @@
+﻿let $ = document
+let locationKey = $.querySelector('#location')
+let title = $.querySelector('title')
+let heading = $.getElementById("heading")
+let titleKeyCode = $.getElementById("keyCode")
+let keyEvent = $.getElementById("key")
+let locationEvent = $.getElementById("location")
+let locantion_name = $.getElementById("locantion-name")
+let whichEvent = $.getElementById("which")
+let codeEvent = $.getElementById("code")
+function keyCodesEvnt(e) {
+	e.preventDefault()
+	
+	keyEvent.innerHTML = e.key
+	heading.innerHTML = `SabzLearn KeyCode ${e.keyCode}`
+	titleKeyCode.innerHTML = e.keyCode
+	locationEvent.innerHTML = e.location
+	whichEvent.innerHTML = e.which
+	codeEvent.innerHTML = e.code
+
+	starter.style.display = 'none'
+	heading.style.display = 'flex'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+}
+
+document.body.addEventListener('keydown', keyCodesEvnt)	
